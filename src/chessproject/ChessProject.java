0d74e7a1a9da9c2e95a2111f3b7d50a21b3895e4
4Shaneob <4Shane@eircom.net>
2016-10-06 12:18:02 +0100

    commit 2

--- src/chessproject/ChessProject.java
+++ src/chessproject/ChessProject.java
@@ -2,7 +2,7 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
- * created by: Keith Maycock and Ralf Bierig, updated for netbeans by Marius Brivinskas 
+ * created by: Keith Maycock and Ralf Bierig, updated for netbeans by Marius Brivinskas.
  */
 package chessproject;
 
